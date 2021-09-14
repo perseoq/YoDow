b56fc04565796db5c77a5d43cd9a5af2c80902e5
Rodrigo <rodrigoqyt@gmail.com>
2021-09-13 20:27:18 -0500

    Update yodow.py

--- yodow.py
+++ yodow.py
@@ -3,25 +3,33 @@ from tkinter import *
 from tkinter.ttk import *
 
 def yodow():
-    def disable():
-        if val.get() == 'ARCHIVO MP3':
-            nv.config(state='disabled')
 
     def formato():
         VIDEO_URL = uv.get()
         VIDEO_NAME = nv.get()
         if val.get() == 'VÍDEO HD':
+            '''
+            Proceso para dar de alta entorno:
+            
+            cd /opt && sudo virtualenv -p pyrhon3 y 
+            sudo chmod 777 -R y
+            source y/bin/activate
+            pip install youtube-dl
+            sudo chmod 755 -R y
+            
+            '''
+            # Ruta del entonro donde se instala el youtube-dl
             for verbose in os.popen(f"/opt/y/bin/youtube-dl -o {VIDEO_NAME} -f best {VIDEO_URL}"):
                 consola.insert('end', verbose)
                 nv.delete(0, END)
                 uv.delete(0, END)
 
         if val.get() == 'ARCHIVO MP3':
+            # Requiere tener instalado ffmpeg (Debian/Ubuntu 'sudo apt install ffmpeg -y')
             for verbose in os.popen(f"/opt/y/bin/youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 -o '{VIDEO_NAME}.%(ext)s' {VIDEO_URL}"):
                 consola.insert('end', verbose)
                 nv.delete(0, END)
                 uv.delete(0, END)
-
 
     v = Tk()
     v.title("Youtube Downloader (& others)")
@@ -30,8 +38,6 @@ def yodow():
 
     fleft = Frame(v)
     fleft.grid(row=0, column=0, padx=5, pady=20)
-    fbotm = Frame(v)
-    fbotm.grid(row=1, column=0)
 
     Label(fleft, text="Nombre del archivo").grid(row=0, column=0)
     Label(fleft, text="URL del video").grid(row=0, column=1)
@@ -51,13 +57,11 @@ def yodow():
     op = OptionMenu(fleft, val, *formatos)
     op.grid(row=1, column=2, sticky="we")
     op.config(width="12")
-    op.bind('<<ListboxSelect>>', disable)
 
     Button(fleft, text="Descargar archivo multimedia", command=formato).grid(row=3, column=0, columnspan=3, sticky="we")
 
     consola = Text(fleft)
     consola.grid(row=4, column=0, columnspan=3, sticky="we")
-    
 
     v.mainloop()
 
